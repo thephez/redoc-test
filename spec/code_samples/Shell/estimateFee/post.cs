108b69212451c0eb50c0257eb285fd00d71e1a08
thephez <thephez@users.noreply.github.com>
2019-06-19 13:13:42 -0400

    Test

--- spec/code_samples/Shell/estimateFee/post.cs
+++
@@ -1,4 +0,0 @@
-curl --request POST \
-  --url http://dapi-testnet.thephez.com:3999/estimateFee \
-  --header 'content-type: application/json' \
-  --data '{"method":"estimateFee","id":1,"jsonrpc":"2.0","params":{"blocks":1}}'
